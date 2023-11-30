83439dda06f6fc765d6b3ee3d7e4657ab20b0a88
naufalrchmns <141924293+naufalrchmns@users.noreply.github.com>
2023-11-30 09:29:31 +0700

    Add files via upload

---
+++ CapstoneWasteClassification/app/src/main/java/com/naufal/capstonewasteclassification/ui/home/HomeViewModel.kt
@@ -0,0 +1,8 @@
+package com.naufal.capstonewasteclassification.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+}
