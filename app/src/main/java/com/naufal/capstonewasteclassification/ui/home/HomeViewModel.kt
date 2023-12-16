f46f3179cff871f08f45445c56affc92e49fc12a
naufalrchmns <naufalrachman27@gmail.com>
2023-12-16 12:44:15 +0700

    App Name Changed

---
+++ app/src/main/java/com/naufal/capstonewasteclassification/ui/home/HomeViewModel.kt
@@ -0,0 +1,6 @@
+package com.naufal.capstonewasteclassification.ui.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+}
--- app/src/main/java/com/naufal/wastelife/ui/menu/MenuViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.naufal.wastelife.ui.menu
-
-import androidx.lifecycle.ViewModel
-
-class MenuViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
