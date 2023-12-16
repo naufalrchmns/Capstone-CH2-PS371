bec09c9d081e6d3146fbcf713fea4fbf9501cc0f
naufalrchmns <141924293+naufalrchmns@users.noreply.github.com>
2023-12-16 10:42:48 +0700

    Delete CapstoneWasteClassification directory

--- CapstoneWasteClassification/app/src/main/java/com/naufal/capstonewasteclassification/ui/home/HomeViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.naufal.capstonewasteclassification.ui.home
-
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class HomeViewModel : ViewModel() {
-}
